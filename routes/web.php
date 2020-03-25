ba18f67fb1d1fee2eb87f961444c076ab9fec73a
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-03-25 18:01:17 +0100

    uso de controladores

--- routes/web.php
+++ routes/web.php
@@ -21,52 +21,72 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 
 //ruta simple
-Route::get('hola', function () {
-    return 'Hola Aaron Agullo';
-});
+Route::get('hola', 'holaController');
 
-Route::get('usuario/{nombre?}', function ($nombre = 'Invitado') {
-    return 'Usuario ' . $nombre;
-})->name('Usuarionombre');
+Route::get('usuario/{nombre?}', 'UsuarioController@usuariounparametro')->name('Usuarionombre');
 
-Route::get('usuario/{nombre}/comentario/{comentarioid}', function ($nombre, $comentarioid) {
-    return 'Usuario ' . $nombre . ' y el comentario es ' . $comentarioid;
-});
+
+
+Route::get('usuario/{nombre}/comentario/{comentarioid}', 'UsuarioController@usuariodosparametros');
+
+
 
 //solo con letras
-Route::get('user/{nombre}', function ($nombre) {
-    return 'Usuario ' . $nombre;
-})->where('nombre', '[A-Za-z]+');
+Route::get('user/{nombre}', 'usuario\UserController@user')->where('nombre', '[A-Za-z]+');
 
 //solo con numeros
-Route::get('user1/{id}', function ($id) {
-    return 'Usuario ' . $id;
-})->where('id', '[0-9]+');
+Route::get('user1/{id}', 'usuario\UserController@user1')->where('id', '[0-9]+');
 
 //con los dos
-Route::get('user2/{id}/{nombre}', function ($id, $nombre) {
-    return 'Usuario ' . $id . ' y el nombre es ' . $nombre;
-})->where(
+Route::get('user2/{id}/{nombre}', 'usuario\UserController@user2')->where(
     [
         'id' => '[0-9]+',
         'nombre' => '[A-Za-z]+'
     ]
 );
 
-Route::get('prueba',function(){
+Route::get('prueba', function () {
     return 'Pagina de prueba';
 })->name('pruebaroute');
 
 
 
-Route::get('redirigirprueba',function(){
+Route::get('redirigirprueba', function () {
     return redirect()->route('pruebaroute');
 });
 
 
-Route::get('redirigirprueba1',function(){
-    return redirect()->route('Usuarionombre',['nombre'=>'Aaron']);
+Route::get('redirigirprueba1', function () {
+    return redirect()->route('Usuarionombre', ['nombre' => 'Aaron']);
 });
 
 
-Route::redirect('/prueba3' ,'/prueba',301);
+Route::redirect('/prueba3', '/prueba', 301);
+
+
+Route::resource('varios', 'variosmetodosrecursos');
+
+Route::resource('varios1', 'variosmetodosrecursos')->only(
+    [
+        'index', 'show'
+    ]
+
+);
+
+Route::resource('varios2', 'variosmetodosrecursos')->except(
+    [
+        'create', 'store', 'update', 'destroy'
+    ]
+
+);
+
+
+
+Route::resource('varios3', 'variosmetodosrecursos')->only(
+    [
+        'index', 'show'
+    ]
+
+)->names([
+    'index'=>'varios.inicio'
+]);
